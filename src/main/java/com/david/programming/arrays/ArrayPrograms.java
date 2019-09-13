464195a95c78bb55c9f43c62d39278cba6fb8084
david <fullstackdahveed@gmail.com>
2019-09-14 01:35:11 +0530

    First commit

---
+++ src/main/java/com/david/programming/arrays/ArrayPrograms.java
@@ -0,0 +1,48 @@
+package com.david.programming.arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class ArrayPrograms 
+{
+    public static void main( String[] args )
+    {
+//    	How to find the missing number in integer array of 1 to 100 ?
+//    	How to find duplicate number on Integer array in Java?'
+//    	How to check if array contains a number in Java?
+//    	How to find largest and smallest number in unsorted array?
+//    	Write a program to remove duplicates from array in Java?
+//    	How find the first repeating element in an array of integers?
+//    	How to reverse array in place in Java?
+
+//    	swap();
+//    	printReverseArray();
+    	
+    }
+
+	private static void printReverseArray() {
+		int [] origArray = {1, 2, 3, 4, 5};
+		int [] reverseArray = new int [origArray.length];
+		int j = 0;
+		
+		for (int i = origArray.length -1 ; i >=0 ; i--) {
+			reverseArray[j] = origArray[i];
+			j ++;
+		}
+		for (int i = 0; i < reverseArray.length; i++) {
+			System.out.print(reverseArray[i]);
+		}
+	}
+
+	private static void swap() {
+		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 3;
+		int temp = a;
+		a = b ;
+		b = temp;
+		System.out.println("a: "+ a+" b : "+b);
+	}
+
+}
---
+++ src/main/java/com/david/programming/string/StringPrograms.java
@@ -0,0 +1,53 @@
+package com.david.programming.string;
+
+/**
+ * Hello world!
+ *
+ */
+public class StringPrograms 
+{
+    public static void main( String[] args )
+    {
+//    	How to Print duplicate characters from String?
+//    	How to check if two Strings are anagrams of each other?
+//    	How to program to print first non repeated character from String?
+//    	How to reverse String in Java using Iteration and Recursion?
+//    	reverseStringUsingStringBuilder();
+//    	reverseStringRecursively();
+//    	How to find duplicate characters in a String?
+    	findDuplicateCharactersInAString();
+//    	How to count a number of vowels and consonants in a String?7. How to count the occurrence of a given character in String?8. How to reverse words in a sentence without using a library method?
+//    	How to return highest occurred character in a String?
+//    	Write a program to remove a given character from String?
+    }
+
+	private static void findDuplicateCharactersInAString() {
+		String str = "Illusion";
+		
+	}
+
+	private static void reverseStringRecursively() {
+		reverseIt("OriginalString");
+		
+	}
+
+	private static void reverseIt(String string) {
+		// TODO Auto-generated method stub
+		if(string.length() ==0) {
+			return;
+		}
+		System.out.print(string.charAt(string.length()-1));
+		reverseIt(string.substring(0, string.length()-1));
+	}
+
+	private static void reverseStringUsingStringBuilder() {
+		// TODO Auto-generated method stub
+		String str = "OriginalString";
+		StringBuilder reverse = new StringBuilder();
+		for (int i = str.length() -1; i >= 0; i--) {
+			reverse.append(str.charAt(i));
+		}
+		System.out.println(reverse);
+		
+	}
+}
